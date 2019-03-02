7909f227da8531f3262053e1ec715269fa7b104f
acolytec3 <a@b.com>
2019-03-01 21:46:20 -0500

    More stuff

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var findAddress = require('../index.js');
 
 describe('findAddress()', function() {
     it('should determine whether there is a string that starts with 0x in a tweet',function(){
-        var tweet = 'hey 0x01325ab3125acbdssq32';
+        var tweet = '@TestOcean, give me some ETH 0x01325ab3125acbdssq32';
         var address = findAddress(tweet);
         expect(address.startsWith('0x')).to.be.true;
     });
