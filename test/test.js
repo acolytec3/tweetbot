9d7a33bd88411ec23a074501d344815aad3ba81c
acolytec3 <a@b.com>
2019-03-02 14:26:22 -0500

    Add negative test for address locator

--- test/test.js
+++ test/test.js
@@ -2,9 +2,15 @@ var expect = require('chai').expect;
 var findAddress = require('../index.js');
 
 describe('findAddress()', function() {
-    it('should determine whether there is a string that starts with 0x in a tweet',function(){
+    it('should identify a phrase that begins with 0x within a tweet and return it',function(){
         var tweet = '@TestOcean, give me some ETH 0x01325ab3125acbdssq32';
         var address = findAddress(tweet);
         expect(address.startsWith('0x')).to.be.true;
     });
+    it('should return a blank if no Ethereum address is found within a tweet',function(){
+        var tweet = '@TestOcean, given me ETH pretty please';
+        var address = findAddress(tweet);
+        expect(address).to.equal('');
+    })
 });
+
