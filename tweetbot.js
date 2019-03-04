e7b00eec26c3d6bea386859b7fd1200568531294
acolytec3 <a@b.com>
2019-03-03 21:17:50 -0500

    More incremental

--- tweetbot.js
+++ tweetbot.js
@@ -1,21 +1,25 @@
 const Twitter = require('twitter');
 const config = require('./config2.js');
 const T = new Twitter(config.twitter);
-var findAddress = require('../index.js').findAddress;
-var getMoney = require('../index.js').getMoney;
+var findAddress = require('./index.js').findAddress;
+var getMoney = require('./index.js').getMoney;
+
+function tweet(user_name, response){
+  if (response.status == 200){
+    T.post('statuses/update',{'status':'@' + user_name + ' coins deposited'})
+  }  else if (error.response.status == '500'){
+      T.post('statuses/update',{'status':'@'+user_name+' '+response.data.error}); 
+  }
+    else {
+      T.post('statuses/update',{'status':'@'+user_name+', something went wrong.  Please check your wallet address and try again later'}); 
+    }
+}
 
 //Twitter listener that listens for mentions of the TestOcean twitter account
 var stream = T.stream('statuses/filter',{track:'@TestOcean'});
 stream.on('data',function(event){
   console.log(event.text);
-  response = getMoney(findAddress(event.text));
-  if (response.status == '200'){
-    T.post('statuses/update',{'status':'@'+event.user.screen_name + ' coins deposited'})
-  }else if (response.status == '500'){
-    T.post('statuses/update',{'status':'@'+event.user.screen_name+' '+response.response.data.error}); 
-  }else {
-    T.post('statuses/update',{'status':'@'+event.user.screen_name+', something went wrong.  Please check your wallet address and try again later'});  
-  }
+  getMoney(findAddress(event.text),tweet);
 });
 
 stream.on('error',function(error){
