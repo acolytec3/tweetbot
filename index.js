bd8b48bf3c473f992b74a81babeacc2a0bf39b67
acolytec3 <konjou@gmail.com>
2019-03-04 20:07:59 -0500

    Refactor code and add additional tests (#2)
    * Incremental
    
    * More incremental
    
    * Commit
    
    * more incremental junk
    
    * raining changes
    
    * Add integration test
    
    * get New money
    
    * Locked, loaded, and tested

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const Twitter = require('twitter');
 const config = require('./config.js');
-const T = new Twitter(config.twitter);
 const axios = require('axios');
 
 // Function to find Ethereum Address in tweet
-function findAddress(message){
+exports.findAddress = function(message){
   var address = message.split(' ');
   for (let i = 0;i < address.length; i++){
     if (address[i].startsWith('0x')){
@@ -15,34 +13,17 @@ function findAddress(message){
 }
 
 // Function to request coins from faucet
-function getMoney(event){
-  axios.post(config.faucet_address,{'address':findAddress(event.text),'agent':'twitter'})
+exports.getMoney = async function(address){
+  return await axios.post(config.faucet_address,{'address':address,'agent':'twitter'})
   .then(function(response){
-    T.post('statuses/update',{'status':'@'+event.user.screen_name + ' coins deposited'})
-    console.log(response.data.status);
+    return({'status':response.status, 'message':'x tokens deposited'})
   })
   .catch(function(error){
-    if (error.response.status == '500'){
-      T.post('statuses/update',{'status':'@'+event.user.screen_name+' '+error.response.data.error}); 
-      console.log(error.response.data)
+    if (error.status == 400){
+    	return ({'status':error.response.status, 'message':'Something.  Please check your wallet address and try again later.'});
+    } else {
+        return ({'status':error.response.status, 'message':error.response.data.error});
     }
-    else {
-      T.post('statuses/update',{'status':'@'+event.user.screen_name+', something went wrong.  Please check your wallet address and try again later'}); 
-       console.log(error.response.data);
-    }
-  });  
+  })
 }
 
-
-//Twitter listener that listens for mentions of the TestOcean twitter account
-var stream = T.stream('statuses/filter',{track:'@TestOcean'});
-stream.on('data',function(event){
-  console.log(event.text);
-  getMoney(event);
-});
-
-stream.on('error',function(error){
-  console.log(error);
-});
-
-module.exports = findAddress, getMoney;
--- test/test.js
+++ test/test.js
@@ -1,6 +1,15 @@
 var expect = require('chai').expect;
-var findAddress = require('../index.js');
+var findAddress = require('../index.js').findAddress;
+var getMoney = require('../index.js').getMoney;
+var axmock = require('axios-mock-adapter');
+var axios = require('axios');
 
+var mock = new axmock(axios);
+
+//###################  Unit Tests  #################################
+mock.onPost().reply(200,{'status':200});
+
+//Unit test for positive scenario where a correct-looking address is provided to function
 describe('findAddress()', function() {
     it('should identify a phrase that begins with 0x within a tweet and return it',function(){
         var tweet = '@TestOcean, give me some ETH 0x01325ab3125acbdssq32';
@@ -13,4 +22,28 @@ describe('findAddress()', function() {
         expect(address).to.equal('');
     })
 });
+
+//Unit test to validate to ensure proper response is created when a correct looking response is provided by stubbed back-end
+//Note: Server response is mocked up in this one
+describe('getMoney()', function(){
+    it('should post a request for coins to the Faucet Server and return a 200 status to calling code when a valid-looking address is provided and a 200 status code is received from server',async() => {
+        var address = '@TestOcean, gimme some ETH 0x185jfae';
+	const response = await getMoney(findAddress(address));
+	expect(response.status).to.equal(200);
+    });
+});
+
+mock.restore();
+
+// #############  Integration Tests  #####################
+//Test to validate to ensure proper response from the faucet server is created when a valid address is provided 
+//Note: The Faucet Server must be configured in config.js for this test to pass
+describe('getMoney()', function(){
+    it('should post a request for coins to the Faucet Server and return a 200 status when a valid address is provided',async() => {
+    var address = '@TestOcean, gimme some ETH 0xa9fe9c78359e841a473fcd54d595c0fd521f4bdb';
+	const response = await getMoney(findAddress(address));
+	expect(response.status).to.equal(200);
+    });
+});
+
 
