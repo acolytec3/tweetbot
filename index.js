a2378befebdc2618f6df4df9df656f168ac88f76
acolytec3 <a@b.com>
2019-03-04 19:41:45 -0500

    get New money

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const config = require('./config.js');
+const config = require('./config2.js');
 const axios = require('axios');
 //const am = require('axios-mock-adapter');
 
@@ -17,15 +17,14 @@ exports.findAddress = function(message){
 exports.getMoney = async function(address){
   return await axios.post(config.faucet_address,{'address':address,'agent':'twitter'})
   .then(function(response){
-//    console.log(response);
-    return({'status':response.data.status, 'message':'x tokens deposited'})
+    return({'status':response.status, 'message':'x tokens deposited'})
   })
   .catch(function(error){
-//    console.log(error);
     if (error.status == 400){
-    	console.log(error.status);
-    	return ({'status':error.status, 'message':'Something.  Please check your wallet address and try again later.'});
-    } else return ({'status':error.status, 'message':error.response.data.error});
+    	return ({'status':error.response.status, 'message':'Something.  Please check your wallet address and try again later.'});
+    } else {
+        return ({'status':error.response.status, 'message':error.response.data.error});
+    }
   })
 }
 
